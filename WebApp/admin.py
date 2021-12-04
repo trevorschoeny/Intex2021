213e1365a7dabe91b461bc399e4063e6c486a2d1
trevorschoeny <84144197+trevorschoeny@users.noreply.github.com>
2021-12-03 21:27:26 -0700

    WORKING MODELS IN ORM!!!

--- WebApp/admin.py
+++ WebApp/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import PdPrescriber,  PdDrugs, PdStatedata, PdCredential, PdSpecialty, PdTriplenew
+from .models import PdPrescriber,  PdDrugs, PdPrescriberDrugs, PdStatedata, PdCredential, PdSpecialty, PdTriplenew
 
 # Register your models here.
 admin.site.register(PdDrugs)
 admin.site.register(PdPrescriber)
+admin.site.register(PdPrescriberDrugs)
 admin.site.register(PdStatedata)
 admin.site.register(PdCredential)
 admin.site.register(PdSpecialty)
